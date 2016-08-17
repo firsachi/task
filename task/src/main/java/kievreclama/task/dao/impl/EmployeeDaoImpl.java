3ddb9b53d3672d9b29b2b3f63276e8f1b7602cd0
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-17 17:31:02 +0300

    Implementation dao file EmployeeDao

---
+++ task/src/main/java/kievreclama/task/dao/impl/EmployeeDaoImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.dao.EmployeeDao;
+import kievreclama.task.dao.hibernate.HibernateUtil;
+import kievreclama.task.entity.Employee;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author firsov
+ */
+public class EmployeeDaoImpl implements EmployeeDao{
+
+    @Override
+    public List<Employee> getList() throws SQLException {
+        List<Employee> resultLst = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession();){
+            resultLst = session.createCriteria(Employee.class).list();
+        }catch (Exception ex){
+            
+        }
+        return resultLst;
+    }
+    
+}
