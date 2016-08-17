7bfd12785914369cca7344db42af4a9ce50669b1
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-17 11:20:49 +0300

    add annotation for hibernate

---
+++ task/src/main/java/kievreclama/task/entity/Urgency.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ *
+ * @author firsov
+ */
+@Entity
+@Table(name = "type_urgency")
+public class Urgency implements Serializable {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+   
+}
