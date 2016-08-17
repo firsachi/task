c2102dd3b1c51da586f9daf21ff51f710cd5f5e2
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-17 17:28:43 +0300

    Add annotation persistence

---
+++ task/src/main/java/kievreclama/task/entity/Employee.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author firsov
+ */
+@Entity
+@Table(name = "emplouers")
+public class Employee implements Cloneable, Serializable{
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    @Column(name = "surname")
+    private String surname;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "patronymic")
+    private String patronymic;
+    
+    @Column(name = "login")
+    @OneToOne
+    @JoinColumn()
+    private String login;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+    
+    public String getLogin() {
+        return login;
+    }
+    
+}
