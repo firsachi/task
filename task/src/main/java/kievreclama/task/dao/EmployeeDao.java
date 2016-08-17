a2328e58e8a798d7689de53e25ef52b8c681d59b
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-17 17:32:24 +0300

    Interface Employee for dao

---
+++ task/src/main/java/kievreclama/task/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.entity.Employee;
+
+/**
+ *
+ * @author firsov
+ */
+public interface EmployeeDao {
+    
+    List<Employee> getList() throws SQLException;
+}
