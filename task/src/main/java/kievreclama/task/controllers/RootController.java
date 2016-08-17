53cc878d59289b793a629ba5caacf9e6c7327991
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-17 17:32:59 +0300

    Main class start jsp pages spring

---
+++ task/src/main/java/kievreclama/task/controllers/RootController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.controllers;
+
+import java.sql.SQLException;
+import kievreclama.task.dao.EmployeeDao;
+import kievreclama.task.dao.impl.EmployeeDaoImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author firsov
+ */
+@Controller
+public class RootController {
+    
+    @RequestMapping(value = "/")
+    public String getPageIndex(){
+        return "index";
+    }
+    
+    @RequestMapping(value = "/ts/")
+    public String getPageTasks(Model model) throws SQLException{
+        EmployeeDao employeeDao = new EmployeeDaoImpl();
+        model.addAttribute("val", employeeDao.getList());
+        return "tasks";
+    }
+    
+}
