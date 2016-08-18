2d897122322519c87f24bb569a2c0502bbeb6b59
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-18 10:48:16 +0300

    Create interface is methods add task delete task find task get list task

---
+++ task/src/main/java/kievreclama/task/dao/TaskDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.entity.Task;
+
+/**
+ *
+ * @author firsov
+ */
+public interface TaskDao {
+    
+    void add() throws SQLException;
+    void delete() throws SQLException;
+    Task find(int id) throws SQLException;
+    List<Task> list() throws SQLException;
+    
+}
