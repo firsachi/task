9d0ecb178f1f80a0cf43467ebdedca51c6c8af6b
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-25 11:53:16 +0300

    Create interface post for dao

---
+++ task/src/main/java/kievreclama/task/dao/PostDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.entity.Post;
+
+/**
+ *
+ * @author firsov
+ */
+public interface PostDao {
+    
+    void add(Post post) throws SQLException;
+    void delete(Post post) throws SQLException;
+    Post find(Integer id) throws SQLException;
+    List<Post> list() throws SQLException;
+}
