3c9752ed6deb9257de2900f3083423767e9efb08
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-26 12:14:17 +0300

    Create entity department

---
+++ task/src/main/java/kievreclama/task/entity/Department.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author firsov
+ */
+@Entity
+@Table(name = "department")
+public class Department implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Basic(optional = false)
+    @Column(name = "name")
+    
+    private String name;
+    @Basic(optional = false)
+    @Column(name = "phone")
+    
+    private String phone;
+    
+    @Basic(optional = false)
+    @Column(name = "fax")
+    private String fax;
+    
+    @Column(name = "delete")
+    private Boolean delete;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public void setDelete(Boolean delete) {
+        this.delete = delete;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public Boolean getDelete() {
+        return delete;
+    }
+    
+    
+}
