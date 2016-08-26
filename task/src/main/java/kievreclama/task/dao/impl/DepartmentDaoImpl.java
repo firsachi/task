548db99c306c5a7952d990de614811415d147a59
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-26 12:39:52 +0300

    Create implements department dao

---
+++ task/src/main/java/kievreclama/task/dao/impl/DepartmentDaoImpl.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.dao.DepartmentDao;
+import kievreclama.task.dao.hibernate.HibernateUtil;
+import kievreclama.task.entity.Department;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
+/**
+ *
+ * @author firsov
+ */
+public class DepartmentDaoImpl implements DepartmentDao{
+
+    @Override
+    public void add(Department department) throws SQLException {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.save(department);
+            session.getTransaction().commit();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Department department) throws SQLException {
+        department.setDelete(true);
+        update(department);
+    }
+
+    @Override
+    public void update(Department department) throws SQLException {
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.update(department);
+            session.getTransaction().commit();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public Department find(Integer id) throws SQLException {
+        Department result = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            result = session.get(Department.class, id);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Department> list() throws SQLException {
+        List<Department> resultList = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession();){
+            Criteria criteria = session.createCriteria(Department.class).addOrder(Order.asc("id"));
+            resultList = criteria.list();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return resultList;
+    }
+    
+}
