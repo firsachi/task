786da4b76ae51da28aadc46b715758f6d06408e0
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-26 16:32:26 +0300

    create mettod submit and getPageForm department

--- task/src/main/java/kievreclama/task/controllers/DepartmentController.java
+++ task/src/main/java/kievreclama/task/controllers/DepartmentController.java
@@ -5,8 +5,18 @@
  */
 package kievreclama.task.controllers;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import kievreclama.task.dao.DepartmentDao;
+import kievreclama.task.dao.impl.DepartmentDaoImpl;
+import kievreclama.task.entity.Department;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -17,7 +27,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DepartmentController {
     
     @RequestMapping
-    public String getPageDepartment(){
+    public String getPageDepartment(Model model) throws SQLException{
+        DepartmentDao departmentDao = new DepartmentDaoImpl();
+        model.addAttribute("departments", departmentDao.list());
         return "department";
+    }
+    
+    @RequestMapping(value = "/form", method = RequestMethod.GET)
+    public ModelAndView getPageForma(@ModelAttribute(name = "id") String id) throws SQLException{
+        Department department = new Department();
+        if (!id.equals("0")){
+            DepartmentDao departmentDao = new DepartmentDaoImpl();
+            department = departmentDao.find(new Integer(id));
+        }
+        ModelAndView model = new ModelAndView("form-department", "department", department);
+        return model;
+    }
+    
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("department")Department department){
+        action(department);
+        return "redirect:../department/";
+    }
+
+    private void action(Department department) {
+        DepartmentDao departmentDao = new DepartmentDaoImpl();
+        if (department.getId() == 0){
+            try {
+                departmentDao.add(department);
+            } catch (SQLException ex) {
+                Logger.getLogger(DepartmentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }else{
+            try {
+                departmentDao.update(department);
+            } catch (SQLException ex) {
+                Logger.getLogger(DepartmentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
