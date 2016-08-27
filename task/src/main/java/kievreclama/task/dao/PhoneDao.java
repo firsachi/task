5fdc18f88227af5dbf69df252d0203fe376bf53c
firsachi <firsachi.alex@gmail.com>
2016-08-27 10:02:29 +0300

    Create phone dao file

---
+++ task/src/main/java/kievreclama/task/dao/PhoneDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.entity.Phone;
+
+/**
+ *
+ * @author firsachi
+ */
+public interface PhoneDao {
+    
+    List<Phone> list() throws SQLException;
+}
