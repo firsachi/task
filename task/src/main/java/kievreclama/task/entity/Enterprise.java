faf2b4ec0d429857231af11682b7e3731ebcea44
firsachi <firsachi.alex@gmail.com>
2016-08-28 21:49:34 +0300

    add attribute employees

--- task/src/main/java/kievreclama/task/entity/Enterprise.java
+++ task/src/main/java/kievreclama/task/entity/Enterprise.java
@@ -6,11 +6,14 @@
 package kievreclama.task.entity;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -32,6 +35,9 @@ public class Enterprise implements Serializable {
     
     @Column(name = "name", unique = true, nullable = false)
     private String name;
+    
+    @OneToMany(mappedBy = "enterprise", fetch = FetchType.LAZY)
+    private List<Employee> employees;
 
     public void setId(Integer id) {
         this.id = id;
@@ -41,12 +47,20 @@ public class Enterprise implements Serializable {
         this.name = name;
     }
 
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+    
     public Integer getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
     }
     
 }
