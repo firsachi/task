92324a5db0ebf1aa41dcb583bea8c0b1265d2c4e
firsachi <firsachi.alex@gmail.com>
2016-08-28 12:09:45 +0300

    add implement one to many employee entity

--- task/src/main/java/kievreclama/task/entity/Department.java
+++ task/src/main/java/kievreclama/task/entity/Department.java
@@ -23,7 +23,7 @@ import javax.persistence.Table;
 public class Department implements Serializable {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Basic(optional = false)
     @Column(name = "id")
     private Integer id;
