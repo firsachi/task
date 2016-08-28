1a2b2c5b4726d49cd75f552297ec0c1c3ed3c20d
firsachi <firsachi.alex@gmail.com>
2016-08-28 12:10:14 +0300

    add implement dao phone

---
+++ task/src/main/java/kievreclama/task/dao/impl/PhoneDaoImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.dao.PhoneDao;
+import kievreclama.task.dao.hibernate.HibernateUtil;
+import kievreclama.task.entity.Phone;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author firsachi
+ */
+public class PhoneDaoImpl implements PhoneDao{
+
+    @Override
+    public List<Phone> list() throws SQLException{
+        List<Phone> resultList = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession();){
+            Criteria criteria = session.createCriteria(Phone.class);
+            resultList = criteria.list();
+        }
+        return resultList;
+    }
+    
+}
