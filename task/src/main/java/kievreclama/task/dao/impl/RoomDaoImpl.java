61bcaca980f3f8f225c3b7e08ff7a7832a4230f5
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-29 10:55:46 +0300

    Create implementtion interfase room dao

---
+++ task/src/main/java/kievreclama/task/dao/impl/RoomDaoImpl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.dao.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+import kievreclama.task.dao.RoomDao;
+import kievreclama.task.dao.hibernate.HibernateUtil;
+import kievreclama.task.entity.Room;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author firsov
+ */
+public class RoomDaoImpl implements RoomDao{
+
+    @Override
+    public List<Room> list() throws SQLException {
+        List<Room> resultList = null;
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Criteria criteria = session.createCriteria(Room.class);
+            resultList = criteria.list();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return  resultList;
+    }
+    
+}
