70892e51cbd91f0fe63234ef0a61dbc404083d02
firsachi <firsov@firsov-w8.kievreclama.local>
2016-08-29 10:46:21 +0300

    Create entity room

---
+++ task/src/main/java/kievreclama/task/entity/Room.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author firsov
+ */
+@Entity
+@Table(name = "room")
+public class Room implements Serializable {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    @Column(name = "number_room", nullable = false, length = 3)
+    private Integer numberRoom;
+    
+    @OneToMany(mappedBy = "employee", fetch = FetchType.LAZY)
+    private List<Employee> employees;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNumberRoom(Integer numberRoom) {
+        this.numberRoom = numberRoom;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getNumberRoom() {
+        return numberRoom;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+    
+    
+}
