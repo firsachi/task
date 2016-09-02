24263b1e8035c29fcc9bf91960d9288d781a8638
firsachi <firsov@firsov-w8.kievreclama.local>
2016-09-02 14:13:57 +0300

    Rename enterprise to company

---
+++ task/src/main/java/kievreclama/task/entity/Company.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kievreclama.task.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author firsachi
+ */
+@Entity
+@Table(name = "company")
+public class Company implements Serializable {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+    
+    @OneToMany(mappedBy = "enterprise", fetch = FetchType.LAZY)
+    private List<Employee> employees;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+    
+}
