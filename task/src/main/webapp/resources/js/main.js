ad7e39869f094349308fc5f29b9538fc90021b91
firsachi <firsachi.alex@gmail.com>
2016-10-25 16:19:42 +0300

    create filter for table employees

--- task/src/main/webapp/resources/js/main.js
+++ task/src/main/webapp/resources/js/main.js
@@ -3,26 +3,59 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/* global room, his */
+/* global room, his, department */
 
 "use strict";
 
 window.onload = function() {
+    if(null === sessionStorage.getItem('room')){
+        sessionStorage.setItem('room', room.selectedIndex);
+    }
+    room.selectedIndex = sessionStorage.getItem('room');
+    if (null === sessionStorage.getItem('department')){
+        sessionStorage.setItem('department', department.selectedIndex);
+    }
+    department.selectedIndex = sessionStorage.getItem('department');
     room.addEventListener('change', function() {
-        this.options[his.selectedIndex].text;
-        console.log(this.options[his.selectedIndex].text);
+        sessionStorage.setItem('room', room.selectedIndex);
+        displayNone();
     });
+    department.addEventListener('change', function(){
+        sessionStorage.setItem('department', department.selectedIndex);
+        displayNone();
+    });
+    displayNone();
 };
-
-
+window.closed = function (){
+    sessionStorage.clear();
+};
 
 function displayNone(){
     var array = document.getElementsByClassName('row-div');
+    var roomNumber = room.options[sessionStorage.getItem('room')].text.trim();
+    var departmentNumber = department.options[sessionStorage.getItem('department')].text.trim();
     for (var index = 1; index < array.length; index++){
-        array[index].style.display = 'table-row';
-        var tmp = array[index].getElementsByTagName('div')[1];
-        if ("210" !== tmp.textContent){
+        var roomColunm = array[index].getElementsByTagName('div')[1];
+        roomColunm = roomColunm.textContent.trim();
+        var departmentColumn = array[index].getElementsByTagName('div')[4];
+        departmentColumn = departmentColumn.textContent.trim();
+        
+        if (check(roomNumber, roomColunm) && check(departmentNumber, departmentColumn)){
+            array[index].style.display = 'table-row';
+        }else{
             array[index].style.display = 'none';
         }
-    }
+    } 
 }
+    
+function check(valueOne, valueTwo){
+    var flag = false;
+    if ("All" === valueOne) {
+        flag = true;
+    } else if (valueOne === valueTwo) {
+        flag = true;
+    }
+    return flag;
+}
+
+
