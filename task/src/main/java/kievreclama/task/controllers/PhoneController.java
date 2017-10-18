81d2c7e672841d8abd3f63ae016c8c79bc33d24e
firsachi <firsachi@mybook.domain.local>
2017-10-18 08:00:58 +0300

    edit

--- task/src/main/java/kievreclama/task/controllers/PhoneController.java
+++ task/src/main/java/kievreclama/task/controllers/PhoneController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,15 +21,6 @@ public class PhoneController {
 		model.setViewName("form-phone-add");
 		model.addObject("phone", new PhoneModel());
 		return model;
-	}
-	
-	@RequestMapping(value = "/chech_login_page", method = RequestMethod.POST)
-	public String checkphoneForm(@Valid @ModelAttribute("phone") Phone phone, BindingResult bindingResult){
-		if (bindingResult.hasErrors()) {
-			return "form-phone-add";
-        }else{
-        	return "home";
-        }
 	}
 	
 	@RequestMapping(value = "save", method = RequestMethod.POST)
