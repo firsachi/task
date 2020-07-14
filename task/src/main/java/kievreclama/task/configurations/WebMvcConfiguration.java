7387005c8361f31d3d99525d9865108a177fda7d
firsachi <firsachi@firsbook.nydomain>
2020-07-14 13:48:17 +0300

    add spring security dialect

--- task/src/main/java/kievreclama/task/configurations/WebMvcConfiguration.java
+++ task/src/main/java/kievreclama/task/configurations/WebMvcConfiguration.java
@@ -1,3 +1,4 @@
+
 package kievreclama.task.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -32,7 +34,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 		templateResolver.setApplicationContext(applicationContext);
 		templateResolver.setPrefix("/WEB-INF/views/");
 		templateResolver.setSuffix(".html");
-		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setCharacterEncoding("UTF-8");		
 		templateResolver.setCacheable(true);
 		return templateResolver;
 	}
@@ -42,6 +44,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(templateResolver());
 		templateEngine.setEnableSpringELCompiler(true);
+		templateEngine.addDialect(new SpringSecurityDialect());
 		return templateEngine;
 	}
 
@@ -49,6 +52,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	public ThymeleafViewResolver viewResolver() {
 		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 		viewResolver.setTemplateEngine(templateEngine());
+		
 		return viewResolver;
 	}
 
