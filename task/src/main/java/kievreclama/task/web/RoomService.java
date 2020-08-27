796cacd03b6b175522c63dbc2fc992588bdbb25f
firsov <firsov@firsov.kievreclama.local>
2020-08-27 09:30:05 +0300

    rename class

--- task/src/main/java/kievreclama/task/web/RoomService.java
+++ task/src/main/java/kievreclama/task/web/RoomService.java
@@ -1,18 +1,27 @@
 package kievreclama.task.web;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kievreclama.task.dao.impl.RoomDaoImpl;
 import kievreclama.task.model.FactoryDao;
 import kievreclama.task.model.dao.RoomDao;
 import kievreclama.task.web.models.RoomModel;
 import kievreclama.task.web.transformers.RoomTransformer;
 
 @Service
-public class ServiceRoom {
+public class RoomService {
 
 	@Autowired
 	private FactoryDao factoryDao;
+	
+	@Autowired
+	private RoomDaoImpl roomDao;
 	
 	@Autowired
 	private RoomTransformer roomTransformer;
@@ -30,6 +39,13 @@ public class ServiceRoom {
 	public void update(RoomModel room) {
 		RoomDao roomDao = factoryDao.createRoomDao();
 		roomDao.upadte(roomTransformer.modelEntity(room));
+	}
+	
+	@Transactional
+	public List<RoomModel> allRoom(String namedQery) {
+		return roomDao.byList(namedQery).stream()
+				.map(room -> roomTransformer.entityModel(room))
+				.collect(Collectors.toList());
 	}
 	
 }
