666fbc5e32f4e5bd5f323006858021c032c64293
firsov <firsov@firsov.kievreclama.local>
2020-11-25 17:11:27 +0200

    adding annotation postmapping

--- task/src/main/java/informer/controller/department/DepartmentAddController.java
+++ task/src/main/java/informer/controller/department/DepartmentAddController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import informer.model.DepartmentModel;
@@ -26,6 +27,7 @@ public class DepartmentAddController extends MainDepartment {
         return "department/department-add";
     }
 	
+	@PostMapping(value = "/add", params = { "save" })
 	public String getSubmitPage(@Valid @ModelAttribute("department") DepartmentModel model,
 			final BindingResult bindingResult) {
 		model.setId(0);
