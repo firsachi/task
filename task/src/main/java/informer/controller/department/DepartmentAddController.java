28fc1f7a6972144e5f360627c036f2107fa6f7be
firsov <firsov@firsov.kievreclama.local>
2020-11-25 17:07:53 +0200

    adding method submit

--- task/src/main/java/informer/controller/department/DepartmentAddController.java
+++ task/src/main/java/informer/controller/department/DepartmentAddController.java
@@ -3,9 +3,15 @@
  */
 package informer.controller.department;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import informer.model.DepartmentModel;
 
 /**
  * @author firsov
@@ -19,5 +25,16 @@ public class DepartmentAddController extends MainDepartment {
     public String getPegeAdd(){
         return "department/department-add";
     }
+	
+	public String getSubmitPage(@Valid @ModelAttribute("department") DepartmentModel model,
+			final BindingResult bindingResult) {
+		model.setId(0);
+		if (bindingResult.hasErrors()) {
+			return "department/department-add";
+		}
+		departmentService.save(model);
+		departmentModel = new DepartmentModel();
+		return "redirect:/departmet/";
+	}
 
 }
