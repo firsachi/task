98643b99e72dd7cfa14dc326780ddc836a3eecf4
firsov <firsov@DESKTOP-SNG7B4F.kievreclama.local>
2022-01-26 10:11:14 +0200

    removing unique

--- task/src/main/java/informer/service/CoreService.java
+++ task/src/main/java/informer/service/CoreService.java
@@ -5,13 +5,11 @@ package informer.service;
 
 import java.util.List;
 
-import informer.validator.unique.FieldValueExists;
-
 /**
  * @author firsov
  *
  */
-public interface CoreService<M> extends FieldValueExists {
+public interface CoreService<M> {
 
 	List<M> all(String namedQery);
 
--- task/src/main/java/informer/validator/unique/FieldValueExists.java
+++
@@ -1,7 +0,0 @@
-package informer.validator.unique;
-
-public interface FieldValueExists {
-
-	boolean fieldValueExists(Object value, String fieldName) throws UnsupportedOperationException;
-	
-}
