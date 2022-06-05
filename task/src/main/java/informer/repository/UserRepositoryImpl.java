5fcedcfae6360c866c88170aeb1a94bc11a2e3b4
firsachi <firsachi@fedora>
2022-06-05 15:28:22 +0300

    new package

--- task/src/main/java/informer/repository/UserRepositoryImpl.java
+++ task/src/main/java/informer/repository/UserRepositoryImpl.java
@@ -9,30 +9,29 @@ import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
-import informer.entity.User;
+import informer.entity.UserApp;
 
 /**
  * @author firsov
  *
  */
 @Repository("userDao")
-public class UserRepositoryImpl extends MainDao<User>{
+public class UserRepositoryImpl extends MainDao<UserApp>{
 
 	@Override
-	public User byId(int id) {
-		return em.find(User.class, id);
+	public UserApp byId(int id) {
+		return em.find(UserApp.class, id);
 	}
 
 	@Override
-	public List<User> byList(String namedQery) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<UserApp> byList(String namedQery) {
+		return em.createNamedQuery(namedQery, UserApp.class).getResultList();
 	}
 
-	public User findByUserName(String username) {
-		TypedQuery<User> query = em.createNamedQuery("User.findUsername", User.class);
+	public UserApp findByUserName(String username) {
+		TypedQuery<UserApp> query = em.createNamedQuery("User.findUsername", UserApp.class);
 		query.setParameter("username", username);
-		User user = query.getSingleResult();
+		UserApp user = query.getSingleResult();
 		return user;
 	}
 
