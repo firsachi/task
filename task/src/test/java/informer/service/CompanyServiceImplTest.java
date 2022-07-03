130f4fdb8b1eaa9d71c5ce3c426773721a13d430
firsachi <firsachi@firs-book>
2022-07-03 09:01:04 +0300

    new folder

--- task/src/test/java/informer/service/CompanyServiceImplTest.java
+++ task/src/test/java/informer/service/CompanyServiceImplTest.java
@@ -13,8 +13,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
-import informer.repository.CompanyDaoImpl;
-import informer.repository.entity.Company;
+import ua.kyiv.informer.logic.CompanyService;
+import ua.kyiv.informer.logic.CompanyServiceImpl;
+import ua.kyiv.informer.repository.CompanyDaoImpl;
+import ua.kyiv.informer.repository.entity.Company;
 
 class CompanyServiceImplTest {
 	
