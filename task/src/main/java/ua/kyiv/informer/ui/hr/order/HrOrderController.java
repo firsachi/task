f5eb0cfa085e8b65376ca95851e2607f20286e89
firsachi <firsachi.alex@gmail.com>
2023-02-20 15:56:22 +0200

    to do

--- task/src/main/java/ua/kyiv/informer/ui/hr/order/HrOrderController.java
+++ task/src/main/java/ua/kyiv/informer/ui/hr/order/HrOrderController.java
@@ -2,19 +2,27 @@ package ua.kyiv.informer.ui.hr.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ua.kyiv.informer.logic.service.HrOrderService;
 
 @Controller
+@RequestMapping(path = {"hr/", "/hr"})
 public class HrOrderController {
 
 	@Autowired
 	private HrOrderService hrOrderService;
 	
-	@RequestMapping(path = {"hr/", "/hr"})
-	public String mainPageHrOrder() {
-		hrOrderService.getHrOrderList();
+	@RequestMapping
+	public String mainPageHrOrder(ModelMap modelMap) {
+		modelMap.addAttribute("hrOrderModelList", hrOrderService.getHrOrderList());
 		return "hr/HrOrder";
+	}
+	
+	@RequestMapping(path = {"/add", "add/"})
+	public String pageAddHrOrder(ModelMap modelMap) {
+		modelMap.addAttribute("hrOrderModel", new HrOrderModel());
+		return "hr/HrOrder-add";
 	}
 }
