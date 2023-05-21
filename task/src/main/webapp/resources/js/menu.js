07c65c6f8709ed8aab2f895cd1d6c7a230043b7a
firsachi <firsachi@firsbook.domain.lan>
2023-05-21 19:12:24 +0300

    rename function set id ul

---
+++ task/src/main/webapp/resources/js/menu.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+ 
+ (() => {
+	steActiveComponentMenuLi();	
+})();
+ 
+ function steActiveComponentMenuLi() {
+	const titletext = $('title').text();
+	$("ul#componentMenu li:contains("+ titletext +")" ).addClass('is-active');
+}
