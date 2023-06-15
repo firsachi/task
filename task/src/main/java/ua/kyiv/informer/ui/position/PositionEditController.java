e74e94c835e82df62a16844ef2450a7be56e6671
firsachi <firsachi.alec@gmail.com>
2023-06-15 16:23:08 +0300

    edit page edit position

--- task/src/main/java/ua/kyiv/informer/ui/position/PositionEditController.java
+++ task/src/main/java/ua/kyiv/informer/ui/position/PositionEditController.java
@@ -19,17 +19,22 @@ import org.springframework.web.bind.annotation.PostMapping;
  */
 @Controller
 public class PositionEditController extends MainPosition {
+
+    @ModelAttribute("nameFragment")
+    public String getNameFragment() {
+        return 	"form-edit";
+    }
 	
     @GetMapping(path = {"/edit/{id}", "edit/{id}"})
     public String getPagesFormCompany( @PathVariable int id, ModelMap model ){
     	model.addAttribute("position", positionService.getId(id));
-        return "position/position-edit";
+        return "position/position";
     }
     
     @PostMapping(path = {"/edit/{id}", "edit/{id}"})
     public String submitUpadtePosition(@Valid @ModelAttribute("position") PositionModel model, BindingResult bindingResult) {
     	if (bindingResult.hasErrors()) {
-			return "position/position-edit";
+			return "position/position";
 		}
     	positionService.update(model);
     	return "redirect:/position/";
