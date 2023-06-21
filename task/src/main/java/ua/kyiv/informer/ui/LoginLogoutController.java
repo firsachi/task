661da63bb2bc3b9f545cb735a16ce70a1b3c986c
firsachi <firsachi.alec@gmail.com>
2023-06-21 14:46:22 +0300

    change package

--- task/src/main/java/ua/kyiv/informer/ui/LoginLogoutController.java
+++ task/src/main/java/ua/kyiv/informer/ui/LoginLogoutController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import ua.kyiv.informer.ui.user.UserModel;
+import ua.kyiv.informer.ui.user.model.UserModel;
 
 /**
  * @author firsachi
