e961984a4b34f9663e2cb89dbeff1c5914b8b7ba
firsachi <firsachi.alex@gmail.com>
2023-07-06 12:35:07 +0300

    edit user form login

--- task/src/main/java/ua/kyiv/informer/ui/LoginLogoutController.java
+++ task/src/main/java/ua/kyiv/informer/ui/LoginLogoutController.java
@@ -1,22 +1,14 @@
-/**
- * 
- */
 package ua.kyiv.informer.ui;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import ua.kyiv.informer.ui.user.model.UserModel;
 
 /**
  * @author firsachi
@@ -24,20 +16,13 @@ import ua.kyiv.informer.ui.user.model.UserModel;
  */
 @Controller
 public class LoginLogoutController {
-
-	@Value("${error.message}")
-	private String errorMessage;
 	
 	@RequestMapping(value = "/login")
-    public String getLoginForm( @RequestParam(value = "error", required = false) String error, Model model) {
-    	model.addAttribute("user", new UserModel());
-    	if(error != null){
-			model.addAttribute("errorMessage", errorMessage);
-		}
+    public String getLoginForm() {
         return "login";
     }
     
-    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    @GetMapping(path = {"/logout", "/logout/"})
     public String logoutPage(HttpServletRequest request, HttpServletResponse response){
     	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
     	if (auth != null){    
