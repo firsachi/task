8fbfdfa3f9b67cc25166528d48c12334e69419f3
firsachi <firsachi.alex@gmail.com>
2023-09-08 14:59:40 +0300

    remove import

--- task/src/main/java/ua/kyiv/informer/ui/department/DepartmentController.java
+++ task/src/main/java/ua/kyiv/informer/ui/department/DepartmentController.java
@@ -1,13 +1,9 @@
 package ua.kyiv.informer.ui.department;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import ua.kyiv.informer.logic.service.DepartmentService;
 
 /**
  *
